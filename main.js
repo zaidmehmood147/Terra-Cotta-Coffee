33eabe28594ff61b77fd30812a7f32c396d4ea49
unknown <kzaid2918@gmail.com>
2025-12-22 22:16:03 +0500

    Added animation and link

---
+++ main.js
@@ -0,0 +1,19 @@
+ScrollReveal().reveal('.reveal', { 
+            distance: '50px', 
+            duration: 1000, 
+            easing: 'ease-in-out',
+            origin: 'bottom',
+            interval: 200 
+        });
+
+        ScrollReveal().reveal('.reveal-left', { 
+            distance: '80px', 
+            duration: 1200, 
+            origin: 'left' 
+        });
+
+        ScrollReveal().reveal('.reveal-right', { 
+            distance: '80px', 
+            duration: 1200, 
+            origin: 'right' 
+        });
